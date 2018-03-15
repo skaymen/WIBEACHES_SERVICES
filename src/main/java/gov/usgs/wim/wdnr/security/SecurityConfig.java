d66c796c31ecd9e24ae6759a50cab5c1bb8aa346
Stanley Kaymen <skaymen@usgs.gov>
2018-03-15 14:40:40 -0500

    add authentication using spring security

---
+++ src/main/java/gov/usgs/wim/wdnr/security/SecurityConfig.java
@@ -0,0 +1,63 @@
+package gov.usgs.wim.wdnr.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    private static String REALM="WIBEACHES";
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select apex_name user_name, apex_psw password, 'Y' enabled from bch_users where apex_name = ? and apex_psw != 'NA'")
+                // sql queries to filter for NA
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .authoritiesByUsernameQuery("select ? user_name, 'valid' authority from dual")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/beachesrawdata", "/version").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic().realmName(REALM).authenticationEntryPoint(basicAuthEntryPoint())
+                .and()
+                .requiresChannel().anyRequest().requiresSecure()
+        ;
+    }
+
+    @Bean
+    BasicAuthenticationEntryPoint basicAuthEntryPoint() {
+        BasicAuthenticationEntryPoint entry = new BasicAuthenticationEntryPoint();
+        entry.setRealmName(REALM);
+        return entry;
+    }
+
+
+
+}
