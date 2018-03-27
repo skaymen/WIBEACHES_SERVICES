6854b3ffc2fbde3b3fe664819b0bf3a6585b3d69
Steinich, Dave R. <drsteini@usgs.gov>
2018-03-27 14:40:56 -0500

    Code Cleanup

--- src/main/java/gov/usgs/wim/wdnr/spring/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package gov.usgs.wim.wdnr.spring;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer {// extends AbstractSecurityWebApplicationInitializer {
-
-}
